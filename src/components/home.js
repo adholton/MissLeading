dfc96f2fc9398f421732cad5ebd3304e8ef9109d
Christina Kurpiel <kurpielcm@gmail.com>
2020-06-04 11:07:17 -0400

    add homepage boilerplate

---
+++ src/components/home.js
@@ -0,0 +1,72 @@
+// import _ from "lodash";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Link } from "react-router-dom";
+// import { fetchPosts } from "../actions";
+
+class Home extends Component {
+  // componentDidMount() {//This is an example of one of React's lifecycle methods
+  //   this.props.fetchPosts();
+  // }
+
+  // renderSearch() {
+  //   return _.map(this.props.posts, post => {
+  //     return (
+  //       <li className="list-group-item" key={post.id}>
+  //         <Link to={`/posts/${post.id}`}>
+  //           {post.title}
+  //         </Link>
+  //       </li>
+  //     );
+  //   });
+  // }
+
+  render() {
+    return (
+      <div>
+      <h1>Can You See Her?</h1>
+        <div className="navigation">
+          <Link className="btn btn-primary" to="/search">
+            Search Movies
+          </Link>
+          <Link className="btn btn-primary" to="/search">
+            Movies that Pass
+          </Link>
+          <Link className="btn btn-primary" to="/search">
+            My List
+          </Link>
+        </div>
+
+        <div className="description">
+          <h3>What is the Bechdel Test?</h3>
+          <p>The Bechdel test is a measure of the representation of women in fiction. It asks three simple questions...</p>
+
+          <ol>
+            <li>Does the movie have two named female characters</li>
+            <li>Do these characters talk to one another?</li>
+            <li>Do they talk about something other than a man?</li>
+          </ol>
+
+          <p>The test is named after American cartoonist Alison Bechdel, who asked these simple questions in her 1985 comic...</p>
+
+          <div className="image-container">
+          <img src="https://nofilmschool.com/sites/default/files/styles/article_wide/public/bechdel_test_-_the_rule.png?itok=eQ6C-S7K" alt="Bechdel test comic strip"></img>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+// function mapStateToProps(state) {//make all of our posts from our Redux Store available to the component's props
+//   return { posts: state.posts };
+// }
+//
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ fetchPosts }, dispatch);
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(PostsIndex);
+export default Home;
--- src/index.js
+++ src/index.js
@@ -7,28 +7,28 @@ import promise from "redux-promise";
 
 import reducers from "./reducers";
 import Home from "./components/home";
-import SearchPage from "./components/search-page";
-import PassingMovies from "./components/passing-movies-page";
+// import SearchPage from "./components/search-page";
+// import PassingMovies from "./components/passing-movies-page";
 
 import 'bootstrap/dist/css/bootstrap.css'
 
 
 //Let's start in index.js by setting up our router, Provider and middleware (we'll be using redux-promise again):
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+// const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 //Notice that we added a route to match the url,
 // /posts/:id, but we also added exact to our root Route just so we can test our new route
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  // <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
         <Switch>
-          <Route path="/search" component={SearchPage} />
-          <Route path="/passing" component={PassingMovies} />
+          {/* <Route path="/search" component={SearchPage} /> */}
+          {/* <Route path="/passing" component={PassingMovies} /> */}
           <Route exact path="/" component={Home} />
         </Switch>
       </div>
-    </BrowserRouter>
-  </Provider>,
+    </BrowserRouter>,
+  // </Provider>,
   document.getElementById('root')
 );
