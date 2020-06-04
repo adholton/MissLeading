271cd1d7d3ffca79530c3589793c7c04d14346c3
ADH <61984231+adholton@users.noreply.github.com>
2020-06-04 11:08:39 -0400

    search page basic boilerplate complete

--- src/components/search-page.js
+++ src/components/search-page.js
@@ -1,59 +1,74 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import { searchMovie } from "../actions";
+//import { connect } from "react-redux";
+//import { bindActionCreators } from "redux";
+//import { fetchMovies } from "../actions";
 import { Link } from "react-router-dom";
 
-class PostsShow extends Component {
-  componentDidMount() {
-    const { id } = this.props.match.params;
-    this.props.fetchPost(id);
+class SearchPage extends Component {
+  constructor(props) {
+    super(props);
+    //console.log(props);
+    //the props is an object that just contains the fetchMovies function 
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onSearchButtonClick = this.onSearchButtonClick.bind(this);
   }
 
-  //Once again we used the params from the URL to figure out which post we're actually wanting to delete. 
-  onDeleteClick() {
-    const { id } = this.props.match.params;
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
 
-    //When using React Router, we now have access to the history package and all it's methods via props. One of those methods is push, which allows us to change our browser's route.
-    //the callback is the anonymous function for routing back to main page
-    this.props.deletePost(id, () => {
-      this.props.history.push("/");
-    });
+  onSearchButtonClick(event) {
+    event.preventDefault();
+
+    // We need to go and fetch movie data based on the search term
+    // the state at this point is already updated from event handler of the user typing, so we invoke the fetch movies function (making the api call) and then resetting the state back back to it's original state (clearing term)
+    this.props.fetchMovies(this.state.term);
+    this.setState({ term: '' });
   }
 
   render() {
-    const { post } = this.props;
-
-    if (!post) {
-      return <div>Loading...</div>;
-    }
-
     return (
       <div>
-        <Link to="/">Back To Index</Link>
-        <button
-          className="btn btn-danger pull-xs-right"
-          onClick={this.onDeleteClick.bind(this)}
-        >
-          Delete Post
-        </button>
-        <h3>{post.title}</h3>
+        <h1 className="heading">Can You See Her?</h1>
+        <h3>Search a movie here to see Bechdel Test Results</h3>
+        <form onSubmit={this.onSearchButtonClick} className="input-group">
+          <input
+            placeholder="Type movie to search here"
+            className="form-control"
+            value={this.state.term}
+            onChange={this.onInputChange}
+          />
+          <span className="input-group-btn">
+            <button type="submit" className="btn btn-secondary">
+              Search</button>
+          </span>
+        </form>
+        <Link to="/">Back to Homepage</Link>
+        {/* <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
-        <p>{post.content}</p>
+        <p>{post.content}</p> */}
       </div>
-    );
+
+    )
   }
 }
 
+export default SearchPage;
+
+// function mapStateToProps({ movies }, ownProps) {
+//   return { movie: movies[ownProps.match.params.id] };
+// }
 
 
-function mapStateToProps({ posts }, ownProps) {
-  return { post: posts[ownProps.match.params.id] };
-}
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ fetchMovies }, dispatch);
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(SearchPage);
 
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({fetchPost, deletePost}, dispatch);
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(PostsShow);
+
