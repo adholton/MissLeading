2b23127aee401756714b3d441e8f5836799df8f9
Christina Kurpiel <kurpielcm@gmail.com>
2020-06-04 10:16:17 -0400

    index.js routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
 
+import reducers from "./reducers";
+import Home from "./components/home";
+import SearchPage from "./components/search-page";
+import PassingMovies from "./components/passing-movies-page";
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+//Let's start in index.js by setting up our router, Provider and middleware (we'll be using redux-promise again):
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+//Notice that we added a route to match the url,
+// /posts/:id, but we also added exact to our root Route just so we can test our new route
 ReactDOM.render(
-  <Provider>
-    <App />
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/search" component={SearchPage} />
+          <Route path="/passing" component={PassingMovies} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
-
-
